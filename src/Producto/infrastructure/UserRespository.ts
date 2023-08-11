393354442917f21300e6a79dc0ebe17ed1bdeaf7
Alanbalza <107290740+Alanbalza@users.noreply.github.com>
2023-08-10 22:48:13 -0600

    cambios

---
+++ src/Producto/infrastructure/UserRespository.ts
@@ -0,0 +1,71 @@
+import { database } from './Database'
+import { Producto } from '../domain/ProducUser'
+import { ProductoRepository } from "../domain/User-repository"
+import { QueryError } from 'mysql2'
+
+export class productoRepositori implements ProductoRepository {
+
+
+  async putUser(Nombre: string, Telefono: string, Correo: string): Promise<Producto> {
+    const mysql = new database();
+    return await new Promise((resolve, reject) => {
+      const sql = `UPDATE Restaurantdb.Users SET Nombre='${Nombre}', Telefono='${Telefono}' WHERE Correo='${Correo}'`;
+      mysql.connection.query(sql, (error: QueryError, results: Producto) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve(results);
+        }
+      });
+    })
+  };
+  async deleteById(Correo: string): Promise<Producto | null> {
+    const mysql = new database();
+    return await new Promise((resolve, reject) => {
+      mysql.connection.query(`DELETE FROM Restaurantdb.Users WHERE Correo = '${Correo}'`, (error: QueryError, rows: Producto) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(rows)
+        }
+      });
+    });
+  }
+  async getById(Correo: string): Promise<Producto | null> {
+    const mysql = new database();
+    return await new Promise((resolve, reject) => {
+      mysql.connection.query(`SELECT * FROM Restaurantdb.Users WHERE Correo = '${Correo}';`, (error: QueryError, rows: Producto) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(rows)
+        }
+      });
+    });
+  }
+  async getUser(): Promise<Producto[] | null> {
+    const mysql = new database();
+    return await new Promise((resolve, reject) => {
+      mysql.connection.query("SELECT * FROM Restaurantdb.Users", (error: QueryError, rows: Producto[]) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(rows)
+        }
+      });
+    });
+  };
+  async create(Nombre: string, Telefono: string, Correo: string): Promise<Producto> {
+    const mysql = new database();
+    return await new Promise((resolve, reject) => {
+      const sql = `INSERT INTO Restaurantdb.Users(Nombre,Telefono,Correo) VALUES ('${Nombre}','${Telefono}','${Correo}')`;
+      mysql.connection.query(sql, (error: QueryError, results: Producto) => {
+        if (error) {
+          reject(error)
+        } else {
+          resolve(results);
+        }
+      });
+    })
+  };
+}
